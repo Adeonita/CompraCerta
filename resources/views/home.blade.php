20d2d5394c04c915d1d0c26a9057d7c2bb90ea52
Renato Andrade <renatoandrade@vipcommerce.com.br>
2021-03-23 21:54:22 -0300

    esboco home

---
+++ resources/views/home.blade.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Compra Certa</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" 
+    integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" 
+    integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous">
+    </script>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" 
+    integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" 
+    integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+
+
+</head>
+<body>   
+    <header>
+        <nav class='navbar navbar-light main w-100'>
+            <div></div>
+            <div>
+                <a href="./login.html" class="btn btn-success"> Entrar</a>
+                <a href='./criarconta.html'class="btn btn-primary"> Criar uma conta</a>
+            </div>
+            <div></div>
+        </nav>
+    </header>
+        <div class='d-flex flex-row justify-content-center'>
+            <a class='mx-2' href="./index.html">
+                <img src="./images/logo.png" alt="CompraCerta" >
+            </a>
+            <div class='my-auto mx-2'>
+                <input type="text" name="" id="">
+                <button class="btn main"">Buscar</button>
+            </div>
+        </div>
+        <div class='departamentos  w-100'>
+            <div class='w-75 h-100 mx-auto d-flex flex-row justify-content-center align-items-center'>
+                <a class='departamento mx-2'>Hortifruti</a>
+                <a class='departamento mx-2'>Eletronicos</a>
+                <a class='departamento mx-2'>Carnes</a>
+            </div>
+        </div>
+        <div>
+            Produtos ...
+        </div>
+        <footer>
+            <nav class='navbar fixed-bottom bg-light'>
+                <div class='mx-auto'> 
+                    Copyright 2021 © CompraCerta HightTecs
+                </div>
+            </nav>
+        </footer>
+</body>
+<style>
+    .main{
+        background-color: #004E98!important;
+        color:white
+    }
+    .departamentos{
+        height:50px;
+        border-top: 2px solid #004E98;
+        border-bottom: 2px solid #004E98;
+    }
+    .departamento{
+        color: #004E98!important; 
+        height: 100%;
+    }
+    .departamento:hover{
+        background-color: #004E98;
+        color: white!important;
+    }
+
+</style>
+
+<script>
+</script>
+
+</html>
