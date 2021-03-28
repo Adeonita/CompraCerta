4480849e2975c49c0046d724ae4150acc8ad3386
adeonita <adeonita.sousa@gmail.com>
2021-03-28 17:52:17 -0300

    feat: add componentization

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,7 @@
+<footer>
+    <nav class='navbar fixed-bottom bg-light'>
+        <div class='mx-auto'> 
+            Copyright 2021 © CompraCerta HightTecs
+        </div>
+    </nav>
+</footer>
---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,19 @@
+<header>
+    <nav class='navbar navbar-light main w-100'>
+        <div></div>
+        <div>
+            <a href="./login.html" class="btn btn-success"> Entrar</a>
+            <a href='./criarconta.html'class="btn btn-primary"> Criar uma conta</a>
+        </div>
+        <div></div>
+    </nav>
+</header>
+<div class='d-flex flex-row justify-content-center'>
+    <a class='mx-2' href="./index.html">
+        <img src="./images/logo.png" alt="CompraCerta" >
+    </a>
+    <div class='my-auto mx-2'>
+        <input type="text" name="" id="">
+        <button class="btn main">Buscar</button>
+    </div>
+</div>
