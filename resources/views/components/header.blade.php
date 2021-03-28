d4fc1385ac5af94f3796a61a8e3c5e83d337e5f1
adeonita <adeonita.sousa@gmail.com>
2021-03-28 18:34:42 -0300

    chore: add url to cart and home

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -4,12 +4,13 @@
         <div>
             <a href="./login.html" class="btn btn-success"> Entrar</a>
             <a href='./criarconta.html'class="btn btn-primary"> Criar uma conta</a>
+            <a href="{{ url('/cart') }}" class="btn btn-primary"> Meu Carrinho</a>
         </div>
         <div></div>
     </nav>
 </header>
 <div class='d-flex flex-row justify-content-center'>
-    <a class='mx-2' href="./index.html">
+    <a class='mx-2' href="{{ url('/home') }}">
         <img src="./images/logo.png" alt="CompraCerta" >
     </a>
     <div class='my-auto mx-2'>
