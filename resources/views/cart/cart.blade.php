e9502ab7b2a32ba7cc58c8706babb1d8a8d2dd86
adeonita <adeonita.sousa@gmail.com>
2021-03-30 22:15:01 -0300

    chore(cart): add div amount

--- resources/views/cart/cart.blade.php
+++ resources/views/cart/cart.blade.php
@@ -1,30 +1,32 @@
 @extends('index')
 @section('content')
-    <table id="my-cart" class="table table-hover mt-5">
-    <thead>
-        <tr>
-        <th scope="col">Quantidade</th>
-        <th scope="col">Produto</th>
-        <th scope="col">Valor unitário</th>
-        <th scope="col">Total</th>
-        </tr>
-    </thead>
-    <tbody>
-    @for($i = 0; $i < 5; $i ++ )
-        <tr>
-        <td class="align-bottom">
-            <p id="amount">1 un</p>
-            <div id="buttons">
-            <button type="button" id="button-less" class="btn btn-outline-primary btn-sm">-</button>
-            <button type="button" id="button-more" class="btn btn-outline-primary btn-sm">+</button>
-            </div>
-        </td>
-        <td scope="row"><img src="{{ asset('images/product.svg') }}" class="rounded float-left product" alt="Produto"> Lorem ipsum dolor sit, amet consectetur adipisicing elit.</td>
-        <td>9,90</td>
-        <td>9,90</td>
-        </tr>
-    @endfor
-    </tbody>
+    <table id="my-cart" class="table  mt-5">
+        <thead>
+            <tr>
+            <th scope="col">Quantidade</th>
+            <th scope="col">Produto</th>
+            <th scope="col">Valor unitário</th>
+            <th scope="col">Total</th>
+            </tr>
+        </thead>
+        <tbody>
+            @for($i = 0; $i < 5; $i ++ )
+                <tr>
+                    <td class="align-bottom">
+                        <div>
+                            <div id="amount">1 un</div>
+                        </div>
+                        <div id="buttons">
+                        <button type="button" id="button-less" class="btn btn-outline-primary btn-sm">-</button>
+                        <button type="button" id="button-more" class="btn btn-outline-primary btn-sm">+</button>
+                        </div>
+                    </td>
+                    <td scope="row"><img src="{{ asset('images/product.svg') }}" class="rounded float-left product" alt="Produto"> Lorem ipsum dolor sit, amet consectetur adipisicing elit.</td>
+                    <td>9,90</td>
+                    <td>9,90</td>
+                </tr>
+            @endfor
+        </tbody>
     </table>
     <div class="row mb-5">
         <div class="col">Subtotal</div>
