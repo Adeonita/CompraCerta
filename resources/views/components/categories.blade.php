05a4fc5d081dcc73f1e293c64b6e13bf9941f458
adeonita <adeonita.sousa@gmail.com>
2021-04-05 12:39:03 -0300

    feat(categories): get data to display the category menu

--- resources/views/components/categories.blade.php
+++ resources/views/components/categories.blade.php
@@ -1,7 +1,13 @@
 <div class='departments  w-100'>
     <div class='w-75 h-100 mx-auto d-flex flex-row justify-content-center align-items-center'>
-        <a class='department mx-2'>Hortifruti</a>
-        <a class='department mx-2'>Eletronicos</a>
-        <a class='department mx-2'>Carnes</a>
+        @php    
+            use App\Http\Controllers\CategoryController;
+
+            $categoryController = new CategoryController(); 
+            $categories = $categoryController->show();
+        @endphp
+        @foreach($categories as $category)
+            <a class='department mx-2'>{{ $category }}</a>
+        @endforeach
     </div>
 </div>
