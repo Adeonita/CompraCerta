c92451b8f70e21cf491d1110b94fa38d66e6dda9
Renato Andrade <renato.emanuel.andrades@gmail.com>
2021-04-08 18:30:48 -0300

    adjusts card product

--- resources/views/components/productCard.blade.php
+++ resources/views/components/productCard.blade.php
@@ -1,15 +1,18 @@
-
-<div class="row">
-    @for($i = 0; $i < 20; $i++)
-    <div class="card product-card mt-5 " style="width: 15rem;">
-      <img class="card-img-top" src="{{ asset('images/feijao.png') }}" alt="Imagem de capa do card">
-      <div class="card-body">
-        <p class="card-text">Produto de teste</p>
-      </div>
-      <div class="row buttons-product-card">
-            <a href="#" class="button-">Comprar</a>
-            <a href="#" class="">Espiar</a>
-      </div>
-    </div>
+<div class="row justify-content-around">
+    @for ($i = 0; $i < 20; $i++)
+        <div class="card product-card col-xs-6 col-sm-4 col-md-4 col-lg-4 px-auto mb-4 mx-2 " style="width: 15rem;">
+            <a href="#">
+                <div>
+                    <img class="card-img-top" src="{{ asset('images/feijao.png') }}" alt="Imagem de capa do card">
+                    <p class="badge price-tag main">R$ 10,99</p>
+                </div>
+            </a>
+            <div class="card-body">
+                <p class="card-text">Produto de teste</p>
+            </div>
+            <div class="row buttons-product-card">
+                <a href="#" class="btn main main-btn">Comprar</a>
+            </div>
+        </div>
     @endfor
 </div>
--- resources/views/home/products.blade.php
+++ resources/views/home/products.blade.php
@@ -1,4 +1,6 @@
 @extends('index')
 @section('content')
+    <div class='container py-2 mb-2'>
         @include('../components/productCard')
+    </div>
 @endsection
