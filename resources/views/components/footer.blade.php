25f2c5f4daaa49fb9a0527410fd26757f87b5b1d
Renato Andrade <renato.emanuel.andrades@gmail.com>
2021-04-22 19:17:55 -0300

    adjust container

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,7 +1,7 @@
 <footer>
     <nav class='navbar fixed-bottom bg-light'>
-        <div class='mx-auto'> 
-            Copyright 2021 © CompraCerta HightTecs
+        <div class='mx-auto'>
+            Copyright 2021 © CompraCerta
         </div>
     </nav>
 </footer>
