009467be99f6a133cffff15435b8138222aeab93
Renato Andrade <renato.emanuel.andrades@gmail.com>
2021-04-29 19:09:45 -0300

    title dashboard

--- resources/views/employee/dashboard.blade.php
+++ resources/views/employee/dashboard.blade.php
@@ -1,6 +1,10 @@
 @extends('index')
 @section('content')
-    <div>
-        Hello You
+    <div class='mb-5'>
+        <div class="my-3">
+            <h3>
+                <i class="bi bi-bar-chart-fill"></i> Dashboard
+            </h3>
+        </div>
     </div>
 @endsection
