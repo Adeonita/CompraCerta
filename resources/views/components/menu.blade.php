8111e67c9993235b5d0cb9328e3aa61e378a033d
adeonita <adeonita.sousa@gmail.com>
2021-05-02 19:28:11 -0300

    refactor(menu): remove remove instance of CategoryController  and add script js

--- resources/views/components/menu.blade.php
+++ resources/views/components/menu.blade.php
@@ -1,15 +1,7 @@
 <div class='departments '>
     <div class='swiper-container h-100'>
-        <div class='w-75 h-100 mx-auto d-flex flex-nowrap swiper-wrapper text-align-center'>
-            @php
-                use App\Http\Controllers\CategoryController;
-
-                $categoryController = new CategoryController();
-                $categories = $categoryController->show();
-            @endphp
-            @foreach ($categories as $category)
-                <a href="#" class="swiper-item department px-3 py-2">{{ $category }}</a>
-            @endforeach
+        <div class='w-75 h-100 mx-auto d-flex flex-nowrap swiper-wrapper text-align-center' id="menu">
         </div>
     </div>
+    <script src="{{ asset('js/menu.js') }}"></script>
 </div>
