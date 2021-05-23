68046a15fd35cef3e289702545212180c1bec7f4
Renato Andrade <renato.emanuel.andrades@gmail.com>
2021-05-23 11:21:07 -0300

    refactor seed database

--- database/seeders/ClientSeeder.php
+++ database/seeders/ClientSeeder.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Str;
 
 use Carbon\Carbon;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\DB;
 
 
@@ -18,13 +19,29 @@ class ClientSeeder extends Seeder
      */
     public function run()
     {
-        DB::table("clients")->insert([
-            "name" => "Luke",
-            "last_name" => "Skywalker",
-            "cpf" =>  "12345678911",
-            "email" => "lukeskywalker" . "@gmail.com",
-            "password" => "123456789",
-            "birth_date" => Carbon::create('2000', '01', '01'),
-        ]);
+
+
+        $clients = [
+            [
+                "name" => "Luke",
+                "last_name" => "Skywalker",
+                "cpf" =>  "12345678911",
+                "email" => "lukeskywalker" . "@gmail.com",
+                "password" => "123456789",
+                "birth_date" => Carbon::create('2000', '01', '01'),
+            ],
+            [
+                "name" => "Lea",
+                "last_name" => "Skywalker",
+                "cpf" =>  "12345678000",
+                "email" => "leaskywalker" . "@gmail.com",
+                "password" => "123456789",
+                "birth_date" => Carbon::create('2000', '01', '01'),
+            ]
+        ];
+
+        foreach ($clients as $client) {
+            DB::table('clients')->insert($client);
+        }
     }
 }
