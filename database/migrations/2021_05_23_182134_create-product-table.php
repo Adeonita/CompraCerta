3d1ab338f74845efd965a5ebdc1af5d47f1fdc8f
adeonita <adeonita.sousa@gmail.com>
2021-05-23 15:55:49 -0300

    feat(products): add product model

---
+++ database/migrations/2021_05_23_182134_create-product-table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+            $table->string("description");
+            $table->float("price");
+            $table->string("image");
+            $table->integer("quantity");
+            $table->unsignedBigInteger("category_id");
+        });
+
+        Schema::table('products', function (Blueprint $table) {
+            $table->foreign("category_id")->references("id")->on("categories");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIdExists('products');
+    }
+}
