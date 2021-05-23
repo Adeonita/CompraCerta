fe9f49ce40746e04fc674e119d48b869ddb7c8da
adeonita <adeonita.sousa@gmail.com>
2021-05-23 16:08:25 -0300

    fix(employee): modify column type from increments to id and add constraint unique to email

--- database/migrations/2021_05_23_025005_create-employee-table.php
+++ database/migrations/2021_05_23_025005_create-employee-table.php
@@ -14,10 +14,10 @@ class CreateEmployeeTable extends Migration
     public function up()
     {
         Schema::create('employees', function(Blueprint $table){
-            $table->increments("id");
+            $table->id();
             $table->string("name");
             $table->string("lastName");
-            $table->string("email");
+            $table->string("email")->unique();
             $table->string("cpf");
             $table->string("password");
             $table->unsignedBigInteger("departments_id");
