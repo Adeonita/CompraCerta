bf503b6084fa962e46d2ccf0ae08465c5d9056d8
Renato Andrade <renato.emanuel.andrades@gmail.com>
2021-06-05 22:30:45 -0300

    add stateseeder to dbseeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,6 +24,8 @@ class DatabaseSeeder extends Seeder
             EmployeeSeeder::class,
             CategorySeeder::class,
             ClientSeeder::class,
+            StateSeeder::class,
+
         ]);
     }
 }
