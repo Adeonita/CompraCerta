e9a5ac3f26e41a88a2a167fecd8ae4a991ce2397
Wesley da Silva e Silva <ede8021@gmail.com>
2021-06-07 13:09:10 -0300

    Adding function to take the list of States in the DB and put in the States box option, function is stil just commented,just  wainting for the migrate fix

--- app/Http/Controllers/StateController.php
+++ app/Http/Controllers/StateController.php
@@ -8,6 +8,15 @@ use App\Models\State;
 class StateController extends Controller
 {
     public function show(){
-        return State::all("name", "initials");
+        
+        $stateList = State::select('id','name')->get();
+        return view('address-option',compact('stateList'));
+
+
+        /*====Colocar no html address-option ESTADOS 
+            @foreach ($stateList as $itens) 
+                <option value="{{$itens->id}}">{{$itens->name}}</option>            
+            @endforeach  
+        */
     }
 }
