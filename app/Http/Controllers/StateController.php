fda62214de6966c39900484c16a5d6a59542cfd3
Wesley da Silva e Silva <ede8021@gmail.com>
2021-06-21 01:21:25 -0300

    taking the list of the states from the DB

--- app/Http/Controllers/StateController.php
+++ app/Http/Controllers/StateController.php
@@ -5,9 +5,18 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\State;
 
+
 class StateController extends Controller
 {
     public function show(){
-        return State::all("name", "initials");
+        
+        $StateList['data'] = State::orderby("name","asc")
+        			   ->select('id','name')
+        			   ->get();
+
+        
+        return view('address-option\address-option')->with("StateList",$StateList);
+        
+        //return State::all("name", "initials");
     }
 }
--- resources/views/address-option/address-option.blade.php
+++ resources/views/address-option/address-option.blade.php
@@ -43,31 +43,10 @@
                     <label for="stateUserRegister" class="form-label">Estado</label><br>
                     <div class="form-group col-6" id="stateUserRegister">
                         <select class="form-control" id="states">
-                        <option value="acre">Acre</option>
-                        <option value="alagoas">Alagoas</option>
-                        <option value="amapa">Amapá</option>
-                        <option value="amazonas">Amazonas</option>
-                        <option value="bahia">Bahia</option>
-                        <option value="ceara">Ceará</option>
-                        <option value="distritoFederal">Distrito Federal</option>
-                        <option value="espiritoSanto">Espírito Santo</option>
-                        <option value="goias">Goiás</option>
-                        <option value="maranhao">Maranhão</option>
-                        <option value="matoGrosso">Mato Grosso</option>
-                        <option value="matoGrossoDoSul">Mato Grosso Do Sul</option>
-                        <option value="minasGerais">Minas Gerais</option>
-                        <option value="para">Pará</option>
-                        <option value="paraiba">Paraíba</option>
-                        <option value="pernambuco">Pernambuco</option>
-                        <option value="piaui">Piauí</option>
-                        <option value="rioDeJaneiro">Rio de Janeiro</option>
-                        <option value="rioGrandeDoSul">Rio Grande do Sul</option>
-                        <option value="rondonia">Rondônia</option>
-                        <option value="roraima">Roraima</option>
-                        <option value="santaCatarina">Santa Catarina</option>
-                        <option value="saoPaulo">São Paulo</option>
-                        <option value="sergipe">Sergipe</option>
-                        <option value="tocantins">Tocantins</option>
+                           <option value='0'>-- Seleione o Estado --</option>
+                            @foreach($StateList['data'] as $rows)
+                                <option value='{{ $rows->id }}'>{{ $rows->name }}</option>
+                            @endforeach
                         </select>
                     </div>
                 </div>
