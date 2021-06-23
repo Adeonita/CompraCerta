9b10fd5db32702e0d0ce12731caab24125978d65
adeonita <adeonita.sousa@gmail.com>
2021-06-23 18:31:56 -0300

    refactor(products): modify iterator from for to foreach

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -1,23 +1,24 @@
 @extends('index')
 @section('content')
     <div class="row justify-content-around">
-        @for ($i = 0; $i < count($products); $i++)
+        @foreach ($products as $item)
             <div class="card product-card col-xs-6 col-sm-4 col-md-4 col-lg-4 px-auto mb-4 mx-2 " style="width: 15rem;">
                 <a href="{{ url('/product') }}">
                     <div>
-                        <img class="card-img-top" src="{{ asset('images/feijao.png') }}" alt="Imagem de capa do card">
+                        <img class="card-img-top" src='{{ asset("images/products/$item->imagePath") }}'
+                            alt="Imagem de capa do card">
                         <div class='price-tags d-flex flex-column align-items-start'>
-                            <p class="badge main">R$ {{ $products[$i]['price'] }}</p>
+                            <p class="badge main">R$ {{ $item->price }}</p>
                         </div>
                     </div>
                 </a>
                 <div class="card-body">
-                    <p class="card-text">{{ $products[$i]['name'] }}</p>
+                    <p class="card-text">{{ $item->name }}</p>
                 </div>
                 <div class="row buttons-product-card">
-                    <a href="{{ url('/product') }}" class="btn main main-btn">Comprar</a>               
+                    <a href="{{ url('/product') }}" class="btn main main-btn">Comprar</a>
                 </div>
             </div>
-        @endfor
-    </div>    
+        @endforeach
+    </div>
 @endsection
