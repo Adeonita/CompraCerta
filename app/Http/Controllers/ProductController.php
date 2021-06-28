6dc3059e0e1ba33a0c327d80f32f67636cb2140d
Renato Andrade <renato.emanuel.andrades@gmail.com>
2021-06-28 09:19:28 -0300

    products home

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -8,43 +8,66 @@ use DB;
 
 class ProductController extends Controller
 {
-    public function show() {
+    public function show()
+    {
         $products = Product::all();
 
-        
+
         $meat = DB::table('products')
-        ->join('categories', 'categories.id', '=', 'products.category_id')
-        ->where("categories.name", "carnes")
-        ->limit(4)
-        ->get();
+            ->join('categories', 'products.category_id', '=', 'categories.id')
+            ->where("categories.name", "carnes")
+            ->select(
+                "products.id",
+                "products.name",
+                "products.price",
+                "products.imagePath",
+            )
+            ->limit(4)
+            ->get();
 
         $frozen = DB::table('products')
-        ->join('categories', 'categories.id', '=', 'products.category_id')
-        ->where("categories.name", "congelados")
-        ->limit(4)
-        ->get();
+            ->join('categories', 'categories.id', '=', 'products.category_id')
+            ->where("categories.name", "congelados")
+            ->select(
+                "products.id",
+                "products.name",
+                "products.price",
+                "products.imagePath",
+            )
+            ->limit(4)
+            ->get();
 
         $cereals = DB::table('products')
-        ->join('categories', 'categories.id', '=', 'products.category_id')
-        ->where("categories.name", "cereais")
-        ->limit(4)
-        ->get();
+            ->join('categories', 'categories.id', '=', 'products.category_id')
+            ->where("categories.name", "cereais")
+            ->select(
+                "products.id",
+                "products.name",
+                "products.price",
+                "products.imagePath",
+            )
+            ->limit(4)
+            ->get();
+
 
         return view("/home/products")->with(
             [
-                "products"=> $products,
+                "products" => $products,
                 "meat" => $meat,
                 "frozen" => $frozen,
                 "cereals" => $cereals,
-            ]);
+            ]
+        );
     }
 
-    public function getByCategory($categoryId) {  
+    public function getByCategory($categoryId)
+    {
         $products = Product::where("category_id", $categoryId)->get();
         return view("/category/index")->with("products", $products);
     }
 
-    public function getByName(Request $request) {
+    public function getByName(Request $request)
+    {
         $search = $_POST['name'];
 
         $products = Product::where('name', 'like', '%' . $search . '%')->get();
@@ -52,7 +75,8 @@ class ProductController extends Controller
     }
 
 
-    public function getById($productId){
+    public function getById($productId)
+    {
         $product = Product::where("id", $productId)->get()[0];
 
         $product->imagePath = "images/products/$product->imagePath";
