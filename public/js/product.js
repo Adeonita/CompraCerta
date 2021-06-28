f0f29997e898e5de41515514d12f0f6577d37b94
Renato Andrade <renato.emanuel.andrades@gmail.com>
2021-06-28 01:48:48 -0300

    redirect login

--- public/js/product.js
+++ public/js/product.js
@@ -2,12 +2,14 @@ const purchaseButton = document.getElementById("purchase-button");
 const localStorage = window.localStorage;
 
 function addItemToCart(product, quantity) {
+
+
     let cart = JSON.parse(localStorage.getItem('cestaCart'));
-    if( !cart ) {
+    if (!cart) {
         cart = []
     }
     const productExist = cart.find(i => i.product.id === product.id)
-    if( productExist ) {
+    if (productExist) {
         const index = cart.indexOf(productExist)
         productExist.quantity = Number(productExist.quantity) + Number(quantity)
         cart[index] = productExist;
@@ -18,11 +20,17 @@ function addItemToCart(product, quantity) {
         })
     }
     localStorage.setItem('cestaCart', JSON.stringify(cart));
+
 }
 
 purchaseButton.addEventListener('click', function() {
-    const product = JSON.parse(purchaseButton.getAttribute("product"));
-    const quantity = document.getElementById("quantity");
-    addItemToCart(product, quantity.value);
-    window.location.href = '/cart'
+    user = getLocalUserId();
+    if (user) {
+        const product = JSON.parse(purchaseButton.getAttribute("product"));
+        const quantity = document.getElementById("quantity");
+        addItemToCart(product, quantity.value);
+        window.location.href = '/cart'
+    } else {
+        window.location.href = '/login'
+    }
 });
