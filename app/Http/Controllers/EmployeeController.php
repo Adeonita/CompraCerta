c52c443760f52f2073fef3c117f1a564d3657c20
adeonita <adeonita.sousa@gmail.com>
2021-07-01 13:58:59 -0300

    feat(employee): add showAllPurchases method

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -4,13 +4,23 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Employee;
-use Tigo\DocumentBr\Cpf;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
+use DB;
 
 class EmployeeController extends Controller
 {
     public function show(){
         return Employee::all();
+    }
+
+    public function showAllPurchases(){
+        $purchases = DB::table('carts')
+        ->join('users', "carts.user_id", '=', 'users.id')
+        ->select(
+            "carts.id",
+            "carts.status",
+            "users.name",
+        )->get();
+
+        return view('employee/purchasingManagement')->with("purchases", $purchases);
     }
 }
