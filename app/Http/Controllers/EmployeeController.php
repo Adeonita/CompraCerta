e2f9600d6b0cadd398aa9e8ddce84cb7748953ca
adeonita <adeonita.sousa@gmail.com>
2021-07-01 18:11:55 -0300

    wip(backoffice): add sendNextDepartment method

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Employee;
+use App\Models\Departments;
+use App\Models\Cart;
+
+
 use DB;
 
 class EmployeeController extends Controller
@@ -22,5 +26,37 @@ class EmployeeController extends Controller
         )->get();
 
         return view('employee/purchasingManagement')->with("purchases", $purchases);
+    }
+
+    private function getNextStatus($status) {
+        switch($status){
+            case 'separação':
+                return 'empacotamento';
+                break;
+
+            case 'empacotamento':
+                return "rota de entrega";
+                break;
+
+            case 'rota de entrega':
+                return "finalizado";
+                break;
+            
+            case 'finalizado':
+                    return "finalizado";
+                    break;
+            
+            default:
+                return "finalizado";
+                break;
+        }
+    }
+
+    public function sendToNextDepartment($status, $cartId) {
+        $statusId = Departments::where("name", $status)->get()[0]['id'];
+        $cart = Cart::where("id", $cartId)
+            ->update([
+                "status" => $this->getNextStatus($status),
+            ]);
     }
 }
