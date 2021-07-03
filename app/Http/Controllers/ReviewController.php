a04b705f7cd4ec0764bc79360c92a0f1e903e4e1
Renato Andrade <renato.emanuel.andrades@gmail.com>
2021-07-03 15:30:44 -0300

    create review controller and review table

---
+++ app/Http/Controllers/ReviewController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Exceptions\InvalidOrderException;
+use Throwable;
+use Illuminate\Support\Facades\Validator;
+
+use Illuminate\Http\Request;
+use App\Models\Review;
+use App\Models\Session;
+use App;
+
+class ReviewController extends Controller
+{
+    public function index()
+    {
+        try {
+
+            $reviews = Review::all();
+
+            return $reviews;
+        } catch (Throwable $e) {
+            return response()->json([
+                'success' => false,
+                'message' => $e
+            ]);
+        }
+    }
+
+    public function getByCart($cartId)
+    {
+        return Review::where("cart_id", $cartId)
+            ->join("cart", "cart.id", "=", "cart_id")
+            ->get();
+    }
+
+    public function setCartReview(Request $request)
+    {
+        // $request->validate([
+        //     "score" => "required",
+        //     "comment" => "required",
+        //     "cart_id" => "required",
+        // ]);
+        try {
+
+            $review = Review::create($request->all());
+            return response()->json(['success' => true, 'message' => $review], 201);
+        } catch (Throwable $e) {
+            return response()->json(['error' => $e], 400);
+        }
+    }
+}
---
+++ database/migrations/2021_07_03_004324_create_reviews_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReviewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+            $table->string("score");
+            $table->string("comment");
+            $table->timestamps();
+
+
+            $table->unsignedBigInteger("cart_id");
+            $table->foreign("cart_id")->references("id")->on("carts")->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reviews');
+    }
+}
