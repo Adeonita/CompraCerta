85395f8ecef36e0273537e1543b89d84dd2d8d88
adeonita <adeonita.sousa@gmail.com>
2021-07-03 19:43:06 -0300

    refactor(user): add new users and new variables

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -47,45 +47,71 @@ class UserSeeder extends Seeder
 
     private function generateClients()
     {
+        $domainUser = "@gmail.com";
+        $domainEmployee = "@compraCerta.com";
+        $password = "123456789";
+        $birthDate = Carbon::create('2000', '01', '01');
+
         $clients = [
             [
                 "name" => "Luke",
                 "last_name" => "Skywalker",
                 "cpf" =>  "12345678911",
-                "email" => "lukeskywalker" . "@gmail.com",
-                "password" => "123456789",
+                "email" => "lukeskywalker" . $domainUser,
+                "password" => $password,
+                "birth_date" => $birthDate,
                 "user_type" => "CLIENT",
-                "birth_date" => Carbon::create('2000', '01', '01'),
-                "department_id" => NULL
+                "department_id" => NULL,
             ],
             [
                 "name" => "Lea",
                 "last_name" => "Skywalker",
                 "cpf" =>  "12345678000",
-                "email" => "leaskywalker" . "@gmail.com",
-                "password" => "123456789",
+                "email" => "leaskywalker" . $domainUser,
+                "password" => $password,
+                "birth_date" => $birthDate,
                 "user_type" => "CLIENT",
-                "birth_date" => Carbon::create('2000', '01', '01'),
-                "department_id" => NULL
+                "department_id" => NULL,
             ],
             [
                 "name" => "Nome",
                 "last_name" => "De teste",
                 "cpf" =>  "12345678913",
-                "email" => "teste" . "@gmail.com",
-                "password" => "123456789",
+                "email" => "teste" . $domainUser,
+                "password" => $password,
+                "birth_date" => $birthDate,
                 "user_type" => "CLIENT",
-                "birth_date" => Carbon::create('2000', '01', '01'),
-                "department_id" => NULL
+                "department_id" => NULL,
             ],
+            [
+                "name" => "Adeonita",
+                "last_name" => "dos Santos",
+                "cpf" =>  "12345678001",
+                "email" => "adeonita" . $domainUser,
+                "password" => $password,
+                "birth_date" => $birthDate,
+                "user_type" => "CLIENT",
+                "department_id" => NULL,
+
+            ], 
+            [
+                "name" => "Renato",
+                "last_name" => "Emanuel",
+                "cpf" =>  "12345678002",
+                "email" => "renato" . $domainUser,
+                "password" => $password,
+                "birth_date" => $birthDate,
+                "user_type" => "CLIENT",
+                "department_id" => NULL,
+            ], 
             [
                 "name" => "Teste Employee",
                 "last_name" => "De Luca",
                 "cpf" =>  "12345678914",
-                "email" => "teste2" . "@gmail.com",
-                "password" => "123456789",
+                "email" => "teste2" . $domainEmployee,
+                "password" => $password,
+                "birth_date" => $birthDate,
                 "user_type" => "EMPLOYEE",
-                "birth_date" => Carbon::create('2000', '01', '01'),
                 "department_id" => 1
             ],
         ];
