c64be561af39322a70cf31dc0ffb214a669c807d
Renato Andrade <renato.emanuel.andrades@gmail.com>
2021-07-04 21:14:50 -0300

    removing dependencies

--- database/seeders/AddressSeeder.php
+++ database/seeders/AddressSeeder.php
@@ -5,10 +5,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use DB;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
-use Carbon\Carbon;
-
 
 class AddressSeeder extends Seeder
 {
